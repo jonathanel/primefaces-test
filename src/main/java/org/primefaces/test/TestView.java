582c41fab65f5d7480666016ef61c6ccc7584872
jonathanel <jonathane@sintecmedia.com>
2017-09-25 20:26:02 +0300

    Update TestView.java

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,19 +1,26 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean(name = "testView")
-@ViewScoped
+@ApplicationScoped
 public class TestView implements Serializable {
     
     private String testString;
     
+	@ManagedProperty("#{carService}")
+	private CarService service;
+    
     @PostConstruct  
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+        cars1 = service.createCars(10);
     }
 
     public String getTestString() {
@@ -22,5 +29,15 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+    
+    private List<Car> cars1;
+ 
+    public List<Car> getCars1() {
+        return cars1;
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
 }
